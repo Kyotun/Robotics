3cc13d634a6567f3182001586a753830d017c914
Kyotun <emirpisirici@hotmail.com>
2025-09-23 06:05:49 +0200

    Exercise5: Change cli.py to world file required True for task4 and task5

--- robotics-exercise5/task5/cli.py
+++ robotics-exercise5/task5/cli.py
@@ -3,7 +3,7 @@ import argparse
 # ----------------- CLI -----------------
 def parse_args() -> argparse.Namespace:
     p = argparse.ArgumentParser(description="Task 4 — Visit-All (UPF + PyRoboSim) with external PDDL files")
-    p.add_argument("--world-file", default="", help="YAML file (under pyrobosim/data) to load a world.")
+    p.add_argument("--world-file", required=True, default="", help="YAML file (under pyrobosim/data) to load a world.")
     p.add_argument("--no-gui", action="store_true", help="Run headless (no GUI).")
     p.add_argument("--engine", default="pyperplan", help="UPF oneshot planner name (e.g., pyperplan, fast-downward, aries).")
     p.add_argument("--robot", default="", help="Robot name to use (default: first robot in world).")
